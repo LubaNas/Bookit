c207cab28e2c0cca3218d1d3b3c43155e5299f1a
LubaNas <58537905+LubaNas@users.noreply.github.com>
2020-02-29 15:48:06 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -3,5 +3,9 @@ package test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Hello, hello!");
+        
+        //practicing fetch and merge 
+      System.out.println("Hello, Fetch and Merge!");
+        
     }
 }
